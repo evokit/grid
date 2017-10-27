cef367b13ee82f13f3d907a30270a18d440a5aa9
e.marchenko <e.marchenko@smartweb.com.ua>
2017-10-27 14:33:17 +0300

    add media prefix for block modifiers

--- rollup.config.js
+++ rollup.config.js
@@ -7,12 +7,34 @@ import postcssVariables from 'postcss-variables';
 import postcssEach from 'postcss-each';
 import postcssConditionals from 'postcss-conditionals';
 import postcssMath from 'postcss-math';
+import postcssMixins from 'postcss-mixins';
+import postcssMqpacker from 'css-mqpacker';
+import postcssNested from 'postcss-nested';
 
 const cssExportMap = {};
 
+const evokitThemeMedia = {
+    name: [
+        'small',
+        'medium',
+        'large',
+        'xlarge',
+        'xxlarge',
+        'xxxlarge'
+    ],
+    value: [
+        'only screen and (min-width: 480px)',
+        'only screen and (min-width: 768px)',
+        'only screen and (min-width: 960px)',
+        'only screen and (min-width: 1200px)',
+        'only screen and (min-width: 1400px)',
+        'only screen and (min-width: 1600px)'
+    ]
+}
 
 const evokitTheme = {
     grid: {
+        media: evokitThemeMedia,
         size: {
             name: ['xxs', 'xs', 's', 'm', 'l', 'xl', 'xxl', 'xxxl'],
             value: [5, 10, 15, 20, 25, 30, 35, 40],
@@ -43,6 +65,9 @@ export default {
                 postcssEach(),
                 postcssConditionals(),
                 postcssMath(),
+                postcssMixins(),
+                postcssNested(),
+                postcssMqpacker(),
                 postcssModules({
                     generateScopedName: function(selector) {
                         return `ui-${selector}`;

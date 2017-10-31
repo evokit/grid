be0a08132d2812b8368ba2f6dd396cc01de1e03c
e.marchenko <e.marchenko@smartweb.com.ua>
2017-10-31 11:00:54 +0200

    move core functions to utils

--- src/index.js
+++ src/index.js
@@ -1,63 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-
-import styles from './style.css';
-
-const ELEMENT_PROP_TYPES = PropTypes.oneOfType([
-    PropTypes.array,
-    PropTypes.string,
-    PropTypes.object,
-    PropTypes.number,
-]);
-
-function getClassName(blockName, prefix, modList, componentProps) {
-    const result = [styles[blockName]];
-
-    for (const key of modList) {
-        const propName = `${prefix}-${key}`;
-
-        if (propName in componentProps) {
-            result.push(
-                classNames(componentProps[propName])
-                    .split(' ')
-                    .map((value) => styles[`${blockName}_${key}_${value}`])
-            );
-        }
-    }
-
-    return classNames(result);
-}
-
-function getPropTypes(prefix, modList) {
-    const result = {};
-
-    if (Array.isArray(modList)) {
-        for (const key of modList) {
-            result[`${prefix}-${key}`] = ELEMENT_PROP_TYPES;
-        }
-    }
-
-    return result;
-}
-
-function createElement(tagName, blockName, modList) {
-    const prefix = blockName.split('__')[0];
-
-    function element(props) {
-        return (
-            React.createElement(tagName, {
-                className: getClassName(blockName, prefix, modList, props),
-            }, props.children)
-        );
-    }
-
-    element.displayName = blockName;
-    element.propTypes = getPropTypes(prefix, modList);
-
-    return element;
-}
-
+import gridStyles from './style.css';
+import { createElement } from './utils';
 
 /**
  * Usage example:
@@ -84,8 +26,22 @@ function createElement(tagName, blockName, modList) {
  *
  * */
 
-const Grid = createElement('div', 'grid', ['column', 'size', 'direction', 'align', 'valign', 'wrap']);
-const GridItem = createElement('div', 'grid__item', ['width', 'order']);
+
+const Grid = createElement({
+    tagName: 'div',
+    blockName: 'grid',
+    prefix: 'grid',
+    modifiers: ['column', 'size', 'direction', 'align', 'valign', 'wrap'],
+    styles: gridStyles,
+});
+
+const GridItem = createElement({
+    tagName: 'div',
+    blockName: 'grid__item',
+    prefix: 'grid',
+    modifiers: ['width', 'order'],
+    styles: gridStyles,
+});
 
 Grid.Item = GridItem;
 
